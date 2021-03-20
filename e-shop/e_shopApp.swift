746e9db602fabcc1a2329a15a6b264fd31829efe
las <crowclaw935@gmail.com>
2021-03-20 20:54:51 +0300

    Initial Commit

---
+++ e-shop/e_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_shopApp.swift
+//  e-shop
+//
+//  Created by Las Rock on 3/20/21.
+//
+
+import SwiftUI
+
+@main
+struct e_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
